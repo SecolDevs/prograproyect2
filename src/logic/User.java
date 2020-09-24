7b882c18dcd892e30a4b0745751a62ed191a4ef6
ErSebah <elyordiusc@gmail.com>
2020-09-24 17:36:19 -0500

    Adding first Functions

---
+++ src/logic/User.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class User {
+
+    private int idUser;
+    private String account, password, typeUser, recoverInfo;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTypeUser() {
+        return typeUser;
+    }
+
+    public void setTypeUser(String typeUser) {
+        this.typeUser = typeUser;
+    }
+
+    public String getRecoverInfo() {
+        return recoverInfo;
+    }
+
+    public void setRecoverInfo(String recoverInfo) {
+        this.recoverInfo = recoverInfo;
+    }
+
+}
---
+++ src/logic/handleData.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class handleData {
+
+    private ArrayList<User> db = new ArrayList<>();
+    private User usuario;
+    private boolean isLoged;
+    private String logedUser;
+
+    public void verifyUser(String[] data) {
+
+    }
+
+    public void addUser(String[] data) {
+
+    }
+
+    public void listUsers() {
+
+    }
+
+    public void findUser() {
+
+    }
+
+    public void loginUser() {
+
+    }
+
+    public void logoutUser() {
+
+    }
+
+    public void restorePass() {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+}
